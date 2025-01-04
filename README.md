64c7b57f767f30f2681386775191e1172eb36965
Peter Dean <peter_john_dean@icloud.com>
2025-01-04 12:39:52 +1100

    Added protocol Comparable along with additional tests

--- README.md
+++ README.md
@@ -14,8 +14,13 @@ I’m not a computer scientist or a pro programmer, but I’m a hobbyist who lov
 ## Introduction
 I will initially creating structured pairs, Int24 and UInt24, leading to a usable library package with the aim to provides some basics like initialise, set, get and provide a description string that will make the values look a little better than the default.
 
-### v0.0.1 - Basics
+### v0.0.1 - Basics with protocols CustomStringConvertible, ExpressibleByIntegerLiteral, ExpressibleByFloatLiteral
 - Initialisers; Int24(), Int24(123), Int24(Int8(-123))
 - Literal types; let a: Int24 = 123, let a: Int24 = -3.14159
 - Descriptions; Int24().description, String("\(Int24())")
 
+### v0.0.2 - Added protocol Comparable
+- The minimum required syntax is to include `==` and `<` operators. 
+- These operators, along with `>`, `>=`, `<=`, and `!=`, are automatically included.
+- While this syntax enables us to work with literals, it does not allow operands to interact with other data types.
+- We will need to include these operators in the next version.
--- Tests/CTInteger24Tests/CTInteger24Tests.swift
+++ Tests/CTInteger24Tests/CTInteger24Tests.swift
@@ -5,7 +5,7 @@ import Foundation
 
 @Test("Initialisation")
 func initialise_tests() async throws {
-    
+    // Note: Before protocol Comparable
     #expect(Int24().value == 0)
     #expect(Int24((UInt24.max + 1) / 2 - 1).value == 8388607)
     
@@ -46,4 +46,33 @@ func initialise_tests() async throws {
     #expect(b3.value == Int24(432.123).value)
     print(a3)
     print(b3)
+}
+
+@Test("Comparable")
+func comparable_tests() async throws {
+    // When I added he Comparable protocol, it only required == and < operands.
+    let a1: Int24 = 1
+    let a2: Int24 = 2
+    let a3: Int24 = 3.14159265358979323846
+    #expect(a1 < a2)
+    #expect(a2 > a1)
+    #expect(a1 <= a2)
+    #expect(a2 >= a1)
+    #expect(a3 == 3)
+    #expect(a3 != 3.5)
+    #expect(Int24(3) == 3)
+    
+    let b1: UInt24 = 1
+    let b2: UInt24 = 2
+    let b3: UInt24 = 3.14159265358979323846
+    #expect(b1 < b2)
+    #expect(b2 > b1)
+    #expect(b1 <= b2)
+    #expect(b2 >= b1)
+    #expect(b3 == 3)
+    #expect(b3 != 3.5)
+    #expect(UInt24(3) == 3)
+    
+    // Interchangable operands of other types need to be included.
+    //#expect(a1 != b1)
 }
